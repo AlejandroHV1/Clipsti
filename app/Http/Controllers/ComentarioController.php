3c03c12e8a7e6a08f3ef07e9e18a598d458f0065
Fausto Hernandez <fausto.fh89@gmail.com>
2024-07-05 09:50:28 -0700

    avance de comentarios

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -1,10 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Comentario;
+use App\Models\Usuario;
 
 use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
-    //
+    public function agregarcomentario(Request $request){
+        $comentario = new Comentario;
+
+        $usuario = session('id');
+
+        $comentario->nombre_com = $request->comentario;
+        $comentario->fk_usuario = $usuario;
+        $comentario->fk_clip = $request->pk_clip;
+
+        $comentario->save();
+
+        return redirect(url('/verclip/'.$request->pk_clip));
+    }
+
+    public function vercomentarios(){
+        
+        $dato_comentario = Comentario::join('clip', 'comentario.fk_clip', '=', 'clip.pk_clip')
+            ->join('usuario', 'comentario.fk_usuario', '=', 'usuario.pk_usuario')
+            ->select('comentario.*', 'usuario.*', 'clip.*')
+            ->where('clip.estatus', '=', '1')
+            ->get();
+
+            return view('verclip',compact("dato_comentario"));
+    }
+    
 }
